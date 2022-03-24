c8ed1fc782ae57378a5ab859502f91f60175eb7a
tsuyosi81 <tharayama@icstars.org>
2022-03-24 01:53:55 -0500

    displayed dice

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScores = 0;
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+    // 1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    // 2. Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    // 3. Check for rolled 1: if true, switch to next player
+    if (dice !== 1) {
+        // Add dice to current score
+        currentScores += dice;
+        current0El.textContent = currentScores; //CHANGE LATER
+    } else {
+        // switch to next player
+    }
+})
