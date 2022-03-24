5aebaf0f48814d6a0175790367ddb51577bd26f9
tsuyosi81 <tharayama@icstars.org>
2022-03-24 12:08:56 -0500

    added the features "holding current scores"

--- starter/script.js
+++ starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--1');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -16,7 +18,17 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScores = 0;
+let activePlayer = 0;
+
+const switchPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScores = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+}
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -31,8 +43,27 @@ btnRoll.addEventListener('click', function () {
     if (dice !== 1) {
         // Add dice to current score
         currentScores += dice;
-        current0El.textContent = currentScores; //CHANGE LATER
+        document.getElementById(`current--${activePlayer}`).textContent = currentScores;
     } else {
         // switch to next player
+        switchPlayer();
     }
+});
+
+btnHold.addEventListener('click', function () {
+    // 1. Add current score to active player's score
+    scores[activePlayer] += currentScores;
+    // scores[1] = scores[1] + currentScores
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+    // 2. Check if player's score is >= 100
+    if (scores[activePlayer] >= 100) {
+        // Finish the game
+        document.querySelector(`player--${activePlayer}`).classList.add('player--winner');
+        document.querySelector(`player--${activePlayer}`).classList.remove('player--active');
+    } else {
+        // Switch to the next player
+        switchPlayer();
+    }
+
 })
